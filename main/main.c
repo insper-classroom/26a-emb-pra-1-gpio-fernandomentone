1a7901102ec5f69304e0ca48252e79a040945f95
fernandomentone <fernandomm9@al.insper.edu.br>
2026-02-25 16:14:08 -0300

    Finalizando

--- main/main.c
+++ main/main.c
@@ -1,17 +1,62 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+const int FASE_1    = 13;
+const int FASE_2 = 12;
+const int FASE_3   = 11;
+const int FASE_4 = 10; 
+const int LED_PIN = 14;
+
+const int BTN_PIN = 15;
+
+static void all_off(void) {
+  gpio_put(FASE_1, 0);
+  gpio_put(FASE_2, 0);
+  gpio_put(FASE_3, 0);
+  gpio_put(FASE_4, 0);
+}
 
 int main() {
-    stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+  stdio_init_all();
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  gpio_init(FASE_1);    gpio_set_dir(FASE_1, GPIO_OUT);
+  gpio_init(FASE_2); gpio_set_dir(FASE_2, GPIO_OUT);
+  gpio_init(FASE_3);   gpio_set_dir(FASE_3, GPIO_OUT);
+  gpio_init(FASE_4); gpio_set_dir(FASE_4, GPIO_OUT);
+
+  all_off();
+
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN  );
+
+  while (true) {
+    if (!gpio_get(BTN_PIN)) {
+        gpio_put(LED_PIN, 1);
+      for (int i = 0; i <= 512; i++) {
+        all_off();
+        gpio_put(FASE_1, 1);
+        sleep_ms(10);
+
+        all_off();
+        gpio_put(FASE_2, 1);
+        sleep_ms(10);
+
+        all_off();
+        gpio_put(FASE_3, 1);
+        sleep_ms(10);
+
+        all_off();
+        gpio_put(FASE_4, 1);
+        sleep_ms(10);
+
+        all_off();
+        }
+    gpio_put(LED_PIN, 0);
     }
+}
 }
